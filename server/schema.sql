cf403e2237dd7f94fae2247d5260f6825da0d2f1
Mevludin Causevic <mevludincausevic@gmail.com>
2023-03-01 17:03:41 -0700

    complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,32 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE user (
+  /* Describe your table here.*/
+  id int NOT NULL,
+  username VARCHAR(10) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE room (
+  /* Describe your table here.*/
+  id int NOT NULL,
+  room VARCHAR(10) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id int NOT NULL,
+  id_user int,
+  body VARCHAR(140) NOT NULL,
+  id_room int,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_user) REFERENCES user(id),
+  FOREIGN KEY (id_room) REFERENCES room(id)
 );
 
 /* Create other tables and define schemas for them here! */
